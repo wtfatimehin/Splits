2e9c3313c975576d7625fb9228e96b725762e841
Willie Fatimehin <willie.fatimehin@gmail.com>
2019-02-23 05:56:21 -0500

    creating Project & Dummy Data

---
+++ Splits/Images+Names+Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Images+Names+Colors.swift
+//  Splits
+//
+//  Created by Willie Fatimehin on 2/23/19.
+//  Copyright © 2019 Willie Fatimehin. All rights reserved.
+//
+
+import UIKit
+
+//Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//Names
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+//Colors
+let colorsArray = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
